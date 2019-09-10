72094ba2a25026fbf7e6a1c6cf26145aeeaa9403
Kevin Li <kevinli4321@gmail.com>
2019-09-10 08:34:52 -0400

    Done

---
+++ xd.py
@@ -0,0 +1,12 @@
+import random
+
+KREWES = {
+    'orpheus': ['Kevin', 'Emily', 'Vishwaa'],
+    'rex': ['William', 'Joseph', 'Calvin'],
+    'endymion': ['Grace', 'Nahi', 'Derek']
+}
+
+def printName(team):
+    print(KREWES[team][random.randint(0, len(KREWES[team]) - 1)])
+
+printName('endymion')
