7c86044b99f430cbc116bb5b4ffdd411dc13834e
pollsmor <kevinli4321@gmail.com>
2019-09-14 11:56:32 -0400

    Added everything into an array for easy picking except the last "total" line is also getting picked up

---
+++ 03_csv/KRLL_LiK-LeeR.py
@@ -0,0 +1,16 @@
+file = open("occupations.csv", "r")
+lines = file.readlines()[1::1]
+occupationsDict = {}
+occupationsArray = []
+
+for line in lines:
+    parsed = line.rsplit(",", 1)
+    jobClass = parsed[0]
+    percentage = float(parsed[1])
+    occupationsDict[jobClass] = percentage
+    for i in range(int(percentage * 10)):
+        occupationsArray.append(jobClass)
+
+file.close()
+
+print(occupationsArray)
