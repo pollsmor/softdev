fa0496e31f9d07d014f7393afcfdc3f44978701f
Kevin Li <kevinli4321@gmail.com>
2019-09-18 08:36:39 -0400

    Three routes with just plain text

---
+++ 08_app0/main.py
@@ -0,0 +1,25 @@
+#Kevin Li
+#SoftDev1 pd1
+#K08 -- Basic Flask app
+#2019-09-18
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("What")
+    return "Hello, world!"
+
+@app.route("/test")
+def test():
+    return "Test, test"
+
+@app.route("/about")
+def about():
+    return "I'm bad at CS:GO"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
