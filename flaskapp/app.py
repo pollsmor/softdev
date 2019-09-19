e6b5fe62e9737a0f0728931acaf2f7fbb00b7c3f
pollsmor <kevinli4321@gmail.com>
2019-09-19 17:14:05 -0400

    Got list to work

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-#Kevin Li - Emily Zheng
+#Kevin Li - Emily Zhang
 #softDev pd1
 #Classwork
 #2019-09-19
@@ -9,9 +9,11 @@ app = Flask(__name__)
 
 list = [1, 2, 3, 4]
 
+lol = "Why isn't this working"
+
 @app.route("/")
 def home():
-    return render_template("my_foist_template/index.html")
+    return render_template("my_foist_template/index.html", list=list)
 
 if __name__ == "__main__":
     app.debug = True
