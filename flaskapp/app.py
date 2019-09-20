04bf1c7ab5caddb7a9a7bab5ee4d882b570beb7d
Kevin Li <kevinli4321@gmail.com>
2019-09-20 08:19:03 -0400

    Foist_template + static stuff

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -7,13 +7,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-list = [1, 2, 3, 4]
+coll = [0, 1, 1, 2, 3, 5, 8]
 
 lol = "Why isn't this working"
 
+#We don't need a route for static stuff
+
 @app.route("/")
 def home():
-    return render_template("my_foist_template/index.html", list=list)
+    return "lol"
+
+@app.route("/my_foist_template")
+def my_foist_template():
+    return render_template("my_foist_template/index.html", foo="Foist Template", list=coll)
 
 if __name__ == "__main__":
     app.debug = True
