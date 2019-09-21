5dac48f364b2770cc6340617ea844b0480f2b511
pollsmor <kevinli4321@gmail.com>
2019-09-21 08:41:06 -0400

    Randomly selected occupation

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -6,13 +6,43 @@
 import random
 from flask import Flask, render_template
 
-app = Flask(__name__)
+#CSV parsing stuff-----------------------------------------------------
+file = open("data/occupations.csv", "r")
+lines = file.readlines()[1:] #skip header line
+lines = lines[:-1] #get rid of total line since that is not useful for this assignment
 
 occupationsArray = []
 
+for line in lines:
+    parsed = line.rsplit(",", 1) #allows me to split from the rightmost comma of a line - avoids issue of an occupation having a comma
+    jobClass = parsed[0]
+    percentage = float(parsed[1]) #must be stored as a number per instructions
+    for i in range(int(percentage * 10)): #Arrays don't take floats as indices and I don't want to chop off the decimal - multiply by 10 to get rid of tenths place
+        occupationsArray.append(jobClass)
+
+file.close() #courtesy? idk I just remember being told to do this in intro2 no matter what
+
+#Randomly chooses an occupation weighted based on percentage
+def pickOccupation():
+    index = random.randint(0, len(occupationsArray) + 1)
+    if (index == len(occupationsArray) or index == len(occupationsArray) + 1):
+        return "Unemployed"
+
+    return occupationsArray[index]
+
+#---------------------------------------------------------------------
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "You're lost lul"
+
 @app.route("/occupyflaskst")
 def occupyflaskst():
-    return render_template("/hw.html")
+    occupation = pickOccupation()
+    print("Randomly picked: " + occupation) #testing if this gets passed
+    return render_template("/hw.html", occupation=occupation)
 
 if __name__ == "__main__":
     app.debug = True
