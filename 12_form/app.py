d64d6d825be1e3d46519686dcd94720c028e7080
pollsmor <kevinli4321@gmail.com>
2019-09-26 18:25:33 -0400

    Done

--- 12_form/app.py
+++ 12_form/app.py
@@ -8,10 +8,13 @@ def home():
 
 @app.route("/auth")
 def auth():
-    print(app)
-    print(request)
-    print(request.args)
-    return request.args["user"]
+    #print(app)
+    #print(request)
+    #print(request.args)
+    #print(request.args["user"])
+    username = request.args['user']
+    method = request.method
+    return render_template("auth.html", username=username, method=method);
 
 if __name__ == "__main__":
     app.debug = True
