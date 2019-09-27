3586931eacd9efb73ded0ce2ad55226428ef3235
pollsmor <kevinli4321@gmail.com>
2019-09-27 18:30:58 -0400

    Test stuff

--- Classwork/9-27/app.py
+++ Classwork/9-27/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from utl import ants
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
@@ -15,6 +17,14 @@ def auth():
     #print(request.args)
     #print(request.args["user"])
     #print(ants.lol())
+    print(request.cookies.get("xd")) #print key test - works even with empty cookie unlike args
+
+    #Testing session stuff
+    session['xd'] = "what"
+    print(session['xd'])
+    session.pop('xd')
+    #print(session['xd']) error
+    
     username = request.args['user']
     method = request.method
     return render_template("auth.html", username=username, method=method);
