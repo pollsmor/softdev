ba2d83bb91a6680bbb764f251cde401e75b46851
pollsmor <kevinli4321@gmail.com>
2019-10-02 18:40:05 -0400

    Root page different behavior

--- 15_login/app.py
+++ 15_login/app.py
@@ -7,24 +7,27 @@ from flask import Flask, render_template, request, session, url_for
 
 app = Flask(__name__)
 
-username = "sleepy";
-password = "sleepier";
+username = 'sleepy';
+password = 'sleepier';
 
 @app.route("/")
 def home():
-    return render_template("root.html");
+    if (session['user']): #checks that a user is logged into a session, render welcome page
+        return render_template("success.html", username=session['user'])
+        
+    return render_template("root.html"); #if not, then render login page
 
 @app.route("/auth", methods=['POST'])
 def auth():
-    username = request.form.get("user")
-    password = request.form.get("pw")
+    username = request.form.get('user')
+    password = request.form.get('pw')
 
-    if (username == "sleepy" and password == "sleepier"):
+    if (username == 'sleepy' and password == 'sleepier'):
         return render_template("success.html", username=username);
 
     else:
-        return render_template("failed.html", reason="username or password")
+        return render_template("failed.html", reason='username or password')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.debug = True
     app.run()
