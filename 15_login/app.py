6431653bd132f5f078c236f8b1c9f5d65eee3cb6
pollsmor <kevinli4321@gmail.com>
2019-10-02 19:13:35 -0400

    Keep session

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,30 +3,40 @@
 # K15 -- Do I Know You?
 # 2019-10-02
 
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, url_for, redirect
+import os
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32)
+
+#Hardcoded combination
 username = 'sleepy';
 password = 'sleepier';
 
 @app.route("/")
 def home():
-    if (session['user']): #checks that a user is logged into a session, render welcome page
+    if (session.get('user')): #checks that a user is logged into a session, render welcome page
+        print("Session username: " + session['user'])
         return render_template("success.html", username=session['user'])
-        
+
     return render_template("root.html"); #if not, then render login page
 
-@app.route("/auth", methods=['POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def auth():
     username = request.form.get('user')
     password = request.form.get('pw')
 
     if (username == 'sleepy' and password == 'sleepier'):
+        session['user'] = username
         return render_template("success.html", username=username);
 
     else:
-        return render_template("failed.html", reason='username or password')
+        return render_template("failed.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
 
 if __name__ == '__main__':
     app.debug = True
--- flaskstarter/app.py
+++ flaskstarter/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, session, url_for
+import os
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
