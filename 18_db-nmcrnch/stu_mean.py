a0237a77c206f1af1af407b09cc9a2943cfb5785
pollsmor <kevinli4321@gmail.com>
2019-10-10 20:59:15 -0400

    Can only get the first guy's average down

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -4,30 +4,53 @@
 #2019-10-10
 
 import sqlite3 #enable control of an sqlite database
-import csv #facilitate CSV I/O
 #import pandas #for printing the db
 
-#Must delete this every time this py file is run!
-DB_FILE = "discobandit.db"
-
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+db = sqlite3.connect('discobandit.db') #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
+#For the table of IDs and associated averages
+db2 = sqlite3.connect('averages.db')
+c2 = db2.cursor()
+
 #==========================================================
-c.execute(
+c2.execute(
     """ CREATE TABLE stu_avg(
-        id INT PRIMARY KEY NOT NULL
+        name TEXT NOT NULL,
+        id INT PRIMARY KEY NOT NULL,
         average NUM NOT NULL
     ) """
 )
 
-print(c.execute("select * from students"))
+id = 1
+while (id <= 10):
+    c.execute(
+        """ SELECT name, students.id, mark
+            FROM students, courses
+            WHERE students.id = courses.id
+            AND students.id = ?
+        """, (id,)
+    )
+
+    total = 0
+    rows = 0
+    name = ""
+    for row in c:
+        name = row[0]
+        total += row[2]
+        rows += 1
+
+    c2.execute("INSERT INTO stu_avg VALUES(?, ?, ?)", name, id, total / rows)
+
+    id += 1
+
 #Printing - requires pandas module
 #print(pandas.read_sql_query("SELECT * from students", db))
-#print('\n')
 #print(pandas.read_sql_query("SELECT * from courses", db))
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
+db2.commit()
+db2.close()
