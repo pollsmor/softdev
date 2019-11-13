07d9a57d53a8023e14df255ce43a5609eb0dec1e
Kevin Li <kevinli4321@gmail.com>
2019-11-13 08:41:08 -0500

    Rick and Morty API

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 @app.route("/countries")
 def countries():
     url = urllib.request.urlopen("https://restcountries.eu/rest/v2/name/united%20states%20of%20america")
@@ -20,6 +20,15 @@ def countries():
     data = json.loads(response)
 
     return render_template("countries.html", flag=data[0]['flag'], name=data[0]['name'])
+
+@app.route("/rick")
+def rick():
+    url = urllib.request.urlopen("https://rickandmortyapi.com/api/character/1")
+    response = url.read()
+    data = json.loads(response)
+
+    return render_template("rick.html", avatar=data['image'], name=data['name'])
+
 
 if __name__ == "__main__":
     app.debug = True
