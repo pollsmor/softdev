394745105d27283d523ba709b992199b2dacf1da
Kevin Li <kevinli4321@gmail.com>
2019-11-13 08:37:51 -0500

    Countries API

---
+++ 25_restrio/app.py
@@ -0,0 +1,26 @@
+#Team {{ }} | Kevin Li & Eric Lau
+#SoftDev1 pd1
+#K25 -- GET more REST
+#2019-11-14
+
+from flask import Flask, render_template
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+@app.route("/countries")
+def countries():
+    url = urllib.request.urlopen("https://restcountries.eu/rest/v2/name/united%20states%20of%20america")
+    response = url.read()
+    data = json.loads(response)
+
+    return render_template("countries.html", flag=data[0]['flag'], name=data[0]['name'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
