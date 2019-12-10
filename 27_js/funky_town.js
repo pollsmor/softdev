7abbd7fb3f60d13776bc90b7ec30c51b0ebdd011
Kevin Li <kevinli4321@gmail.com>
2019-12-10 18:37:15 -0500

    Comments

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,7 +1,6 @@
 var factorial = function(n) {
-  if (n < 2) {
+  if (n < 2)
 		return 1;
-	}
 
 	return n * factorial(n - 1);
 };
@@ -25,18 +24,18 @@ var min = function(a, b) {
 }
 
 var gcd = function(a, b) {
-	if (a % b == 0)
+	if (a % b == 0) //can early end the function if a is a factor of b
 		return b;
 
-	else if (b % a == 0)
+	else if (b % a == 0) //vice versa pf above
 		return a;
 
-	var minimum = min(a, b);
-	while (a % minimum != 0 || b % minimum != 0 || minimum == 1) {
+	var minimum = min(a, b); //count down from here to find gcd
+	while (a % minimum != 0 || b % minimum != 0 || minimum == 1) { //min = 1 to cancel the loop
 		minimum--;
 	}
 
-	if (minimum <= 0)
+	if (minimum <= 0) //if the factor is indeed 1 then -- would've made it 0.
 		return 1;
 
 	return minimum;
@@ -45,6 +44,6 @@ var gcd = function(a, b) {
 var list = ["Kevin", "Yevgeniy", "Jesse", "Pratham"]; //for testing randomStudent()
 
 var randomStudent = function() {
-  var idx = Math.floor(Math.random() * list.length);
+  var idx = Math.floor(Math.random() * list.length); //random: 0 to 1, floor: nearest int rounded down
   return list[idx];
 }
