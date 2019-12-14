63711508bde086f169a81c6e3ed6290b34b9ea20
Kevin Li <kevinli4321@gmail.com>
2019-12-13 21:28:21 -0500

    Got fib2 working

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -31,6 +31,9 @@ var addItem = function(e) {
     var list = document.getElementById("thelist");
     var item = document.createElement("li");
     item.innerHTML = "WORD";
+    item.addEventListener('mouseover', changeHeading);
+    item.addEventListener('mouseout', changeHeading);
+    item.addEventListener('click', removeItem);
     list.appendChild(item);
   }
 };
@@ -39,6 +42,7 @@ var button = document.getElementById("b");
 button.addEventListener('click', addItem);
 
 var fib = function(n) {
+  if (n === 0) return 0;
   if (n < 2) return 1;
   return fib(n - 1) + fib(n - 2);
 };
@@ -54,17 +58,36 @@ var addFib = function(e) {
   }
 };
 
-/*
-var addFib2 = function(e) {
-  console.log(e);
-  ???
-  ...see QAF re: DYNAMIC PROGRAMMING...
-  ???
-};
-*/
-
 var fb = document.getElementById("fb");
 fb.addEventListener('click', addFib);
+
+var fib2 = function(n) {
+  var fibs = [0, 1];
+
+  if (n === 0 || n === 1)
+    return n;
+
+  for (var i = 2; i <= n + 1; i++) {
+    fibs.push(fibs[i - 2] + fibs[i - 1]);
+  }
+
+  return fibs[n];
+}
+
+var fib2Input = 0;
+var addFib2 = function(e) {
+  //console.log(e);
+  if (e.type === 'click') {
+    var list = document.getElementById("fib2list");
+    var item = document.createElement("li");
+    item.innerHTML = fib2(fib2Input++).toString();
+    list.appendChild(item);
+
+  }
+};
+
+var fb2 = document.getElementById("fb2");
+fb2.addEventListener('click', addFib2);
 
 var fact = function(n) {
   if (n < 2)
