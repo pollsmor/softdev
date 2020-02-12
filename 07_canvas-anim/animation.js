5df3ed1319d2fce2c336222e3470ef68b4f9271f
Kevin Li <kevinli4321@gmail.com>
2020-02-12 16:32:24 -0500

    Thing works now

--- 07_canvas-anim/animation.js
+++ 07_canvas-anim/animation.js
@@ -4,7 +4,7 @@
 //2020-02-12
 
 var animateButton = document.getElementById('animate');
-var stop = document.getElementById('stop');
+var stopButton = document.getElementById('stop');
 
 var canvas = document.getElementById('playground');
 const ctx = canvas.getContext('2d');
@@ -12,22 +12,39 @@ var width = canvas.width;
 var height = canvas.height;
 var centerX = width / 2;
 var centerY = height / 2;
-var maxRadius = 150;
+var maxRadius = 250;
 var currRadius = 0;
+var incRadius = 1;
 
 //ctx.arc(centerX, centerY, 10, 0, 2 * Math.PI);
 //ctx.stroke();
 
-var animationID = 0;
+var animationID;
 
-var animate = function(e) {
+var animate = function() {
   animationID = requestAnimationFrame(animate);
-  console.log("aaaaa");
-  ctx.arc(centerX, centerY, currRadius, 0, 2 * Math.PI);
-  ctx.stroke();
+  console.log("currRadius: " + currRadius);
   ctx.clearRect(0, 0, width, height);
+
+  if (incRadius) {
+    currRadius++;
+    if (currRadius > maxRadius)
+      incRadius = 0;
+  } else {
+    currRadius--;
+    if (currRadius < 1)
+      incRadius = 1;
+  }
+
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, currRadius, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
 animateButton.addEventListener("click", function(e) {
   requestAnimationFrame(animate);
 });
+
+stopButton.addEventListener("click", function(e) {
+  cancelAnimationFrame(animationID);
+})
