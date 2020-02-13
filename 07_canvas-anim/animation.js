a2185dca6668a2ba1f425b0bb134586000c63222
Kevin Li <kevinli4321@gmail.com>
2020-02-13 09:32:49 -0500

    End of class

--- 07_canvas-anim/animation.js
+++ 07_canvas-anim/animation.js
@@ -1,24 +1,28 @@
 //Kevin Li & William Lin
 //SoftDev2 pd2
-//K07 -- Canvas Animation
-//2020-02-12
+//K08 -- Canvas Animation 2
+//2020-02-13
 
-var animateButton = document.getElementById('animate');
-var stopButton = document.getElementById('stop');
 var canvas = document.getElementById('playground');
 const ctx = canvas.getContext('2d');
+var expandButton = document.getElementById('animate');
+var dvdButton = document.getElementById('dvd');
+var stopButton = document.getElementById('stop');
+var expandRunning = false;
+var dvdRunning = false;
 
-var maxRadius = 250;
+//----------------------------------------------------------
+
+var expandAnimationID;
+var maxRadius = canvas.width / 2 - 10;
 var currRadius = 0;
 var incRadius = true;
 
-var animationID = 0;
-var animationRunning = false; //to know whether clicking the animate button should do anything
-
-var animate = function() {
-  animationID = requestAnimationFrame(animate);
+var expand = function() {
+	cancelAnimationFrame(expandAnimationID);
+  expandAnimationID = requestAnimationFrame(expand);
   //console.log("currRadius: " + currRadius); //lags the console if it's open
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the screen so that a new frame can be drawn
 
   if (incRadius) {
     currRadius++;
@@ -35,14 +39,47 @@ var animate = function() {
   ctx.fill();
 }
 
-animateButton.addEventListener("click", function(e) {
-  if (!animationRunning) {
-    animationRunning = true;
-    requestAnimationFrame(animate);
+//----------------------------------------------------------
+
+var dvdAnimationID;
+var dvdX;
+var dvdY;
+var direction = Math.floor(Math.random(2 * Math.PI)); //360 degrees of movement
+
+var dvdBounce = function() {
+	cancelAnimationFrame(dvdAnimationID);
+	dvdAnimationID = requestAnimationFrame(dvdBounce);
+	if (!dvdRunning) {
+		dvdX = Math.floor(Math.random(canvas.width - 30));
+		dvdY = Math.floor(Math.random(canvas.height - 30));
+	}
+
+	dvdRunning = true;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	dvdX += Math.sin(direction);
+	dvdY += Math.sin(direction);
+
+	ctx.beginPath();
+	ctx.arc(dvdX, dvdY, 5, 0, 2 * Math.PI);
+	ctx.fill();
+}
+
+expandButton.addEventListener("click", function(e) {
+  if (!expandRunning) {
+    expandRunning = true;
+    requestAnimationFrame(expand);
   }
 });
 
+dvdButton.addEventListener("click", function(e) {
+	dvdRunning = false; //need this for the conditional in dvdBounce
+	requestAnimationFrame(dvdBounce);
+})
+
 stopButton.addEventListener("click", function(e) {
-  animationRunning = false;
-  cancelAnimationFrame(animationID);
+  expandRunning = false;
+	dvdRunning = false;
+  cancelAnimationFrame(expandAnimationID);
+	cancelAnimationFrame(dvdAnimationID);
 })
---
+++ 08_canvas-anim/animation.js
@@ -0,0 +1,49 @@
+//Kevin Li & William Lin
+//SoftDev2 pd2
+//K07 -- Canvas Animation
+//2020-02-12
+
+var animateButton = document.getElementById('animate');
+var stopButton = document.getElementById('stop');
+var canvas = document.getElementById('playground');
+const ctx = canvas.getContext('2d');
+
+var maxRadius = canvas.width / 2 - 10;
+var currRadius = 0;
+var incRadius = true;
+
+var animationID = 0;
+var animationRunning = false; //to know whether clicking the animate button should do anything
+
+var animate = function() {
+	cancelAnimationFrame(animationID);
+  animationID = requestAnimationFrame(animate);
+  //console.log("currRadius: " + currRadius); //lags the console if it's open
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the screen so that a new frame can be drawn
+
+  if (incRadius) {
+    currRadius++;
+    if (currRadius > maxRadius)
+      incRadius = false;
+  } else {
+    currRadius--;
+    if (currRadius < 1)
+      incRadius = true;
+  }
+
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, currRadius, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+animateButton.addEventListener("click", function(e) {
+  if (!animationRunning) {
+    animationRunning = true;
+    requestAnimationFrame(animate);
+  }
+});
+
+stopButton.addEventListener("click", function(e) {
+  animationRunning = false;
+  cancelAnimationFrame(animationID);
+})
