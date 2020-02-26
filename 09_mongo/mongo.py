6aa61e1800a9caa3e7bababb8e15879cfb1fee4d
Kevin Li <kevinli4321@gmail.com>
2020-02-25 19:21:20 -0500

    Get by zip and grade

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -31,7 +31,9 @@ def get_by_zip_and_grade(zipcode, grade):
     client = MongoClient('poggers-cc.ddns.net', 27017)
     db = client['test']
     collection = db['restaurants']
-    cursor = collection.find({"address.zipcode": zipcode})
+
+    #grades.0 gets the latest inspection grade, since I don't care that a restaurant is A in the past if it's C now.
+    cursor = collection.find({"address.zipcode": zipcode, "grades.0.grade": grade})
 
     for document in cursor:
         print(document)
@@ -40,3 +42,4 @@ def get_by_zip_and_grade(zipcode, grade):
 
 #get_by_borough("Bronx")
 #get_by_zip("10462")
+#get_by_zip_and_grade("10462", "B")
