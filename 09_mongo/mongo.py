c65edffd5337a3753023b705c764cb736bb25cfc
Kevin Li <kevinli4321@gmail.com>
2020-02-25 19:12:57 -0500

    Atom why u no push

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -1,3 +1,8 @@
+#Kevin Li & Hillary Zen
+#SoftDev pd2
+#K09 -- Mongo Basics
+#2020-02-25
+
 from pymongo import MongoClient
 
 def get_by_borough(borough):
@@ -22,5 +27,16 @@ def get_by_zip(zipcode):
 
     client.close()
 
+def get_by_zip_and_grade(zipcode, grade):
+    client = MongoClient('poggers-cc.ddns.net', 27017)
+    db = client['test']
+    collection = db['restaurants']
+    cursor = collection.find({"address.zipcode": zipcode})
+
+    for document in cursor:
+        print(document)
+
+    client.close()
+
 #get_by_borough("Bronx")
-get_by_zip("10462")
+#get_by_zip("10462")
