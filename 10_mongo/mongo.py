8296a60c63d1a6f8882fbb3e3d7ae0a0311a6901
Kevin Li <kevinli4321@gmail.com>
2020-02-29 11:51:56 -0500

    Import data into database

--- 10_mongo/mongo.py
+++ 10_mongo/mongo.py
@@ -1,11 +1,12 @@
-#Team {undefined} | Kevin Li & Jason Zheng
+#Team Rocket | Kevin Li & Jason Zheng
 #SoftDev pd2
 #K10 -- Mongo Import/Export
 #2020-02-29
 
-server_address = '127.0.0.1'
+server_address = 'poggers-cc.ddns.net'
 
 from pymongo import MongoClient
+import json
 
 """
 def get_by_borough(borough):
@@ -19,3 +20,16 @@ def get_by_borough(borough):
 
     client.close()
 """
+
+def create_db():
+    client = MongoClient(server_address, 27017)
+    db = client['rocket']
+    collection = db['pokedex']
+
+    file = open("pokedex.json", 'r')
+    data = json.load(file)['pokemon']
+    collection.insert_many(data)
+
+    client.close()
+
+create_db()
