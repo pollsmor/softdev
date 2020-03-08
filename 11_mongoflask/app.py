570686519cc74ea592e9a31a58b1d5d152b144c7
Kevin Li <kevinli4321@gmail.com>
2020-03-08 15:22:10 -0400

    Query code (need to test on droplet next)

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -7,17 +7,30 @@ from flask import Flask, render_template, request
 from utl import mongo_ops
 
 app = Flask(__name__)
-#mongo_ops.create_db()
+mongo_ops.create_db()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title = "Mongo Frontend")
 
 @app.route("/results")
 def results():
-    return "lol"
+    type = request.args.get('type')
+    minheight = request.args.get('minheight')
+    minweight = request.args.get('minweight')
+    dualtype = request.args.get('dualtype')
+    pokemon = request.args.get('pokemon')
+
+    if dualtype == 'true':
+        title = "Dual-typed Pokemon"
+        query = mongo_ops.get_dual_typed()
+
+    if pokemon:
+        title = ("Pokemon strong against: " + pokemon)
+
+    return render_template("results.html", title = title, query = query)
 
 if __name__ == "__main__":
     app.debug = True
-    #app.run(host='0.0.0.0')
-    app.run()
+    app.run(host='0.0.0.0')
+    #app.run()
