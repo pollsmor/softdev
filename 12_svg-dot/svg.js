46ee8ad721d388c2aa2327f326139bc9ec32a519
Kevin Li <kevinli4321@gmail.com>
2020-03-27 09:34:27 -0400

    Clear button

--- 12_svg-dot/svg.js
+++ 12_svg-dot/svg.js
@@ -1,4 +1,14 @@
 var pic = document.getElementById("vimage");
+var clearbutton = document.getElementById("clear");
+
+var clear = function() {
+  //false means the svg is cloned but not its children (i.e. the circles inside),  replace with clone
+  pic.parentNode.replaceChild(pic.cloneNode(false), pic);
+}
+
+//<svg id="vimage" height="500" width="500" style="border: 1px solid;">
+
+/*
 var c = document.createElementNS(
   "http://www.w3.org/2000/svg", "circle");
 
@@ -9,3 +19,6 @@ c.setAttribute("fill", "red");
 c.setAttribute("stroke", "black");
 
 pic.appendChild(c);
+*/
+
+clearbutton.addEventListener("click", clear);
