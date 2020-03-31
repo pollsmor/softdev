4fb89ba87e3c8678d4c659d731ec7983a6386c13
Kevin Li <kevinli4321@gmail.com>
2020-03-31 13:00:41 -0400

    Not sure why the circles are sticking to the corners rather than bouncing

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -1,55 +1,73 @@
-//We Suck At Calculus - Kevin Li / Derek Leung (Big Buford)
-//SoftDev pd2
-//K13 -- Ask Circles
-//2020-03-30
+//No Ivies Club - Kevin Li / Raymond Lee
+//SoftDev pd2/pd1
+//K14 -- Ask Circles While Moving
+//2020-03-31
 
 var namespace = "http://www.w3.org/2000/svg"; //don't want to keep retyping this
 var pic = document.getElementById("vimage");
+var animationID;
+var directions = [-1, 1]; //pick one of the two for directionX and directionY: -1 for left and up, 1 for right and down.
+
 var clearbutton = document.getElementById("clear");
+var movebutton = document.getElementById("move");
+var xtrabutton = document.getElementById("xtra");
 
 var clear = function() {
-  var rect = document.createElementNS(namespace, "rect");
-  rect.setAttribute("width", 500);
-  rect.setAttribute("height", 500);
-  rect.setAttribute("style", "fill:rgb(255,255,255);"); //big brain maneuvers
-  pic.appendChild(rect);
+  var curr = pic.firstChild;
+  while (curr) {
+    pic.removeChild(curr);
+    curr = pic.firstChild;
+  }
 }
 
 var draw = function(e) {
-  var x = e.offsetX;
-  var y = e.offsetY;
+  if (e.target == pic) {
+    var x = e.offsetX;
+    var y = e.offsetY;
 
-  var c = document.createElementNS(namespace, "circle");
-  c.setAttribute("cx", x);
-  c.setAttribute("cy", y);
-  c.setAttribute("r", 20);
-  c.setAttribute("fill", "blue");
+    var c = document.createElementNS(namespace, "circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", 20);
+    c.setAttribute("fill", "blue");
 
-  c.addEventListener("click", draw2);
-  pic.appendChild(c);
+    pic.appendChild(c);
+  }
 }
 
-var draw2 = function(e) {
-  this.setAttribute("fill", "cyan");
-  this.addEventListener("click", draw3);
-  e.stopPropagation(); //stops parent listeners from being called (aka. don't make a new circle in draw() )
+var move = function(e) {
+  cancelAnimationFrame(animationID);
+	animationID = requestAnimationFrame(move);
+
+  var circles = pic.children;
+  for (var i = 0; i < circles.length; i++) {
+    var directionX, directionY;
+    var moving = circles[i].getAttribute("moving");
+    if (moving !== "true") { //don't want it to change directions once it has started moving
+      circles[i].setAttribute("moving", "true");
+      directionX = directions[Math.floor(Math.random() * 2)]; //choose to go left or right
+  		directionY = directions[Math.floor(Math.random() * 2)]; //choose to go up or down
+      circles[i].setAttribute("directionX", directionX);
+      circles[i].setAttribute("directionY", directionY);
+    }
+
+    directionX = parseInt(circles[i].getAttribute("directionX"));
+    directionY = parseInt(circles[i].getAttribute("directionY"));
+    var x = parseInt(circles[i].getAttribute("cx"));
+    var y = parseInt(circles[i].getAttribute("cy"));
+
+    circles[i].setAttribute("cx", x + directionX); //horizontal movement; is -1 or 1
+    circles[i].setAttribute("cy", y + directionY); //vertical movement
+
+  	if (x <= 20 || x >= 480) circles[i].setAttribute("directionX", directionX * -1); //reverse direction upon reaching a corner
+  	if (y <= 20 || y >= 480) circles[i].setAttribute("directionY", directionY * -1);
+  }
 }
 
-var draw3 = function(e) {
-  var x = Math.floor(Math.random() * 500);
-  var y = Math.floor(Math.random() * 500);
-
-  var c = document.createElementNS(namespace, "circle");
-  c.setAttribute("cx", x);
-  c.setAttribute("cy", y);
-  c.setAttribute("r", 20);
-  c.setAttribute("fill", "blue");
-
-  c.addEventListener("click", draw2);
-  pic.appendChild(c); //add new blue circle
-  pic.removeChild(this); //remove cyan circle
-  e.stopPropagation(); //don't call draw and draw2 again
-}
-
+//------------------------------------------------------------------------------
 clearbutton.addEventListener("click", clear);
 pic.addEventListener("click", draw);
+
+movebutton.addEventListener("click", function(e) {
+  requestAnimationFrame(move);
+});
