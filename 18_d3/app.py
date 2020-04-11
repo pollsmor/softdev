7c94ea6add252edaa79e002aff242b29c1f33d80
Kevin Li <kevinli4321@gmail.com>
2020-04-11 15:31:59 -0400

    Transfer data from Flask to JS file

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from utl import read_csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html");
+    return render_template("index.html", data = read_csv.read_csv());
 
 if __name__ == "__main__":
     app.debug = True
--- 18_d3/static/transition.js
+++ 18_d3/static/transition.js
@@ -2,3 +2,12 @@
 //SoftDev2 pd2
 //K18: D3 in Flask
 //2020-04-08
+
+var renderButton = document.getElementById("render");
+var transitionButton = document.getElementById("transition");
+
+var render = function() {
+  console.log(data['nyc']);
+}
+
+renderButton.addEventListener("click", render);
